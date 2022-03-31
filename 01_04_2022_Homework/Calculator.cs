24e66866b1dd108e56051571728fdf11923f077f
Ershad Teymurlu <7ahgfnr@code.edu.az>
2022-03-31 21:44:00 +0400

    01.04.2022 task added

---
+++ 01_04_2022_Homework/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_04_2022_Homework
+{
+    internal static class Calculator
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Divide(int a, int b)
+        {
+            return (double)a / (double)b;
+        }
+        public static int Pow(this int variable, int a)
+        {
+            int pow = 1;
+            for (int i = 1; i <= a; i++) pow = pow * variable;
+            return pow;
+        }
+
+    }
+}
---
+++ 01_04_2022_Homework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_04_2022_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("value of the varibale 'a': ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("value of the varibale 'b': ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($@"Sum: {Calculator.Sum(a, b)}
+Subtract: {Calculator.Subtract(a, b)}
+Multiply: {Calculator.Multiply(a, b)}
+Divide: {Calculator.Divide(a, b)}
+Power of a: {a.Pow(2)}
+Power of b: {b.Pow(4)}");
+            Console.ReadLine();
+        }
+    }
+}
